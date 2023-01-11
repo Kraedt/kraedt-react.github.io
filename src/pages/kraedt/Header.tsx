2318d857e49148695fa0cce15cb5c8d6ffbc5730
Chris <cschwartz110@gmail.com>
2023-01-10 22:16:15 -0600

    fix description and remove twitter link

--- src/pages/kraedt/Header.tsx
+++ src/pages/kraedt/Header.tsx
@@ -3,7 +3,7 @@ import soundcloudIcon from '../../assets/images/soundcloud_icon.png';
 import youtubeIcon from '../../assets/images/youtube_icon.png';
 import spotifyIcon from '../../assets/images/spotify_icon.png';
 import beatportIcon from '../../assets/images/beatport_icon.png';
-import twitterIcon from '../../assets/images/twitter_icon.png';
+//import twitterIcon from '../../assets/images/twitter_icon.png';
 import kraedtLogo from '../../assets/images/logo.png';
 import { Link, useLocation } from 'react-router-dom';
 import { useState } from 'react';
@@ -37,7 +37,7 @@ export const Header = () => {
     <ListLinkIcon url="http://youtube.com/kraedt" img={youtubeIcon} title="YouTube" alt="youtube" />
     <ListLinkIcon url="https://open.spotify.com/artist/0YbhxZi9PSVTmB4UMkM5Jw" img={spotifyIcon} title="Spotify" alt="spotify" />
     <ListLinkIcon url="https://beatport.com/artist/kraedt/429462" img={beatportIcon} title="Beatport" alt="beatport" />
-    <ListLinkIcon url="http://twitter.com/kraedt" img={twitterIcon} title="Twitter" alt="twitter" />
+    {/*<ListLinkIcon url="http://twitter.com/kraedt" img={twitterIcon} title="Twitter" alt="twitter" />*/}
   </>
 
   return (
