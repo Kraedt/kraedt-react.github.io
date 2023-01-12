ec2eb7ca636192cf1d4458e6feb348d17899300e
Chris Schwartz <cschwartz110@gmail.com>
2023-01-12 14:55:47 -0600

    smarter music item image get

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -139,7 +139,7 @@ export const externalLink = (title: string, url?: string, img?: string) => {
 
 const convertDriveUrlToId = (rawUrl: string) => rawUrl.substring(rawUrl.search("id="), rawUrl.search("&")).slice(3);
 export const getDriveImage = (url: string) => `https://drive.google.com/uc?export=view&id=${url.startsWith("http") ? convertDriveUrlToId(url) : url}`
-export const getMusicItemImage = (item: any) => item.imageUrl?.startsWith("http") ? getDriveImage(item.imageUrl) : (isNullOrWhitespace(item.imageUrl) ? NoImage : (songImages[item.imageUrl] ?? NoImage));
+export const getMusicItemImage = (item: any) => item.imageUrl?.startsWith("http") ? getDriveImage(item.imageUrl) : (isNullOrWhitespace(item.imageUrl) ? NoImage : (songImages[item.imageUrl] ?? getDriveImage(item.imageUrl)));
 
 export const SpotifyLink = (url?: string) => externalLink('Spotify', url, 'spotify.png')
 export const ItunesLink = (url?: string) => externalLink('iTunes', url, 'itunes.png')
